9402796ef233ad2ccc0f66cddc7089ddcfed252a
João Roberto B. da Silva <jobond46@gmail.com>
2018-02-02 12:39:35 -0400

    Criando arquivos e tal

---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+include "main.php";
+
+include "footer.php";
+?>
--- public/main.php
+++ public/main.php
@@ -1,3 +1,4 @@
+<?php
 <!doctype html>
 <html lang="pt-br">
 
@@ -83,51 +84,4 @@
                     </div>
                 </div>
             </nav>
-            <!--Início do conteúdo da dashboard-->
-            <div class="content">
-                            
-            </div>
-            <!--Fim do conteúdo da dashboard-->
-            <footer class="footer">
-                <div class="container-fluid">
-                    <p class="copyright pull-right">
-                        © 2018 Sebrae API, desenvolvido na Campus Party XI!
-                    </p>
-                </div>
-            </footer>
-        </div>
-    </div>
-</body>
-<!--   Core JS Files   -->
-<script src="../assets/js/jquery-3.2.1.min.js" type="text/javascript"></script>
-<script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="../assets/js/material.min.js" type="text/javascript"></script>
-<!--  Charts Plugin -->
-<script src="../assets/js/chartist.min.js"></script>
-<!--  Dynamic Elements plugin -->
-<script src="../assets/js/arrive.min.js"></script>
-<!--  PerfectScrollbar Library -->
-<script src="../assets/js/perfect-scrollbar.jquery.min.js"></script>
-<!--  Notifications Plugin    -->
-<script src="../assets/js/bootstrap-notify.js"></script>
-<!--  Google Maps Plugin    -->
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
-<!-- Material Dashboard javascript methods -->
-<script src="../assets/js/material-dashboard.js?v=1.2.0"></script>
-<!-- Material Dashboard DEMO methods, don't include it in your project! -->
-<script src="../assets/js/demo.js"></script>
-<script type="text/javascript">
-    $(document).ready(function () {
-
-        // Javascript method's body can be found in assets/js/demos.js
-        demo.initDashboardPageCharts();
-
-    });
-
-    function changeSelection(e){
-        $('#horizontal-menu li').removeClass('active');
-        $(e).addClass('active')
-    }
-</script>
-
-</html>
+            ?>
